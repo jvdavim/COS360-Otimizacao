b7164d24e823609525e233d1dd892e8e4ea9e0f3
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2017-11-17 07:16:09 -0200

    Change vector to valarray
    Changing vector to valarray.

--- script.cpp
+++ script.cpp
@@ -1,43 +1,16 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <vector>
+#include <valarray>
 
 using namespace std;
 
-
-vector<double> soma(vector<double>& a, vector<double>& b){
-    if (a.size() != b.size()) return -1;
-    vector<double> resultado;
-    for (int i = 0; i < a.size(); i++){
-        resultado.push_back(a[i]+b[i]);
-    }
-    return resultado;
-}
-
-vector<double> multiplica_escalar(int t, vector<double>& x){
-    vector<double> resultado;
-    for (int i = 0; i < x.size(); i++){
-        resultado.push_back(t*x[i]);
-    }
-    return resultado;
-}
-
-double multiplica_vetor(vector<double>& a, vector<double>& b){
-    if (a.size() != b.size()) return -1;
-    double resultado;
-    for (int i = 0; i < a.size(); i++){
-        resultado += a[i]*b[i];
-    }
-    return resultado;
-}
-
-double f(vector<double>& x){
+double f(valarray<double>& x){
     // Funcao objetivo
     return x[0]**3 + x[1]**3 + x[2]**3;
 }
 
-vector<double> grad(vector<double>& x){
+valarray<double> grad(valarray<double>& x){
     vector<double> resultado;
     resultado.push_back(3*(x[0]**2));
     resultado.push_back(3*(x[1]**2));
