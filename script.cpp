b1d81d5007eaf3a42d5a17f485e08118961d382e
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2017-11-17 18:10:19 -0200

    Fix gradiente
    Some problems fixed. Code cleared. Algorithm didn't converge.

--- script.cpp
+++ script.cpp
@@ -2,12 +2,16 @@
 #include <valarray>
 
 
-double f(std::valarray<double>& x){
+double norma(std::valarray<double> x){
+    return sqrt(pow(x[0], 2) + pow(x[1], 2) + pow(x[2], 2));
+}
+
+double f(std::valarray<double> x){
     // Funcao objetivo
     return pow(x[0], 3) + pow(x[1], 3) + pow(x[2], 3);
 }
 
-std::valarray<double> grad(std::valarray<double>& x){
+std::valarray<double> grad(std::valarray<double> x){
     // Funcao gradiente de f() no ponto x
     std::valarray<double> resultado (3);
     resultado[0] = 3*pow(x[0], 2);
@@ -16,43 +20,34 @@ std::valarray<double> grad(std::valarray<double>& x){
     return resultado;
 }
 
-double armijo(std::valarray<double>& x, std::valarray<double>& d, double g, double n ){
+double armijo(std::valarray<double> x, std::valarray<double> d, double g, double n ){
     // Funcao de armijo. Retorna um tamanho de passo
     double t = 1;
-    std::valarray<double> arg = x + t*d;
-    std::cout << "Chamei o armijo" << std::endl;
-    while (f(arg) > f(x) + n*t*((grad(x)*d).sum())){
-        std::cout << "Armijo t = " << t << std::endl;
+    while (f(x + t*d) > f(x) + n*t*((grad(x)*d).sum())){
         t = g*t;
-        arg = x + t*d;
     }
     return t;
 }
 
-std::valarray<double> gradiente(std::valarray<double>& x){
-    int k = 0;
-    double epslon = 0.00927;
-    std::valarray<double> px = x;
-    while (grad(px).sum() > epslon){
-        std::cout << "x = ( " << grad(x)[0] << ", " << grad(x)[1] << ", " << grad(x)[2] << " )" << std::endl;
-        std::cout << "grad(px).sum() = " << grad(px).sum() << std::endl;
-        std::valarray<double> d = grad(px)*(-1.0);
-        std::cout << "-grad(px).sum() = " << d.sum() << std::endl;
-        double t = armijo(px, d, 0.8, 0.25);
-        std::cout << "t = " << t << std::endl;
-        x = px + t*d;
-        k++;
-        px = x;
-        getchar();
+std::valarray<double> gradiente(std::valarray<double> x){
+    double epslon = 0.000000927;
+    double t = 1;
+    std::valarray<double> d;
+    while (t > epslon){
+        d = grad(x)*(-1.0);
+        t = armijo(x, d, 0.8, 0.25);
+        std::cout<<t<<std::endl;
+        x = x + t*d;
+        // getchar();
     }
     return x;
 }
 
 int main(){
-double initx[] = {0, -1, 1};
-// double initd[] = {1, 0, 0};
+double initx[] = {3, 2, 8};
+// double initd[] = {-1, 0};
 std::valarray<double> x (initx, 3);
-// std::valarray<double> d (initd, 3);
+// std::valarray<double> d (initd, 2);
 // std::cout << armijo(x, d, 0.8, 0.25) << std::endl;
 std::cout << "( " << gradiente(x)[0] << ", " << gradiente(x)[1] << ", " << gradiente(x)[2] << " )" << std::endl;
 return 0;
